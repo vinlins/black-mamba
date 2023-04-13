c81a8ddb43390563b40ac24d8a0e132a0d4e53b4
vinlins <vinlins@yahoo.com>
2023-04-13 18:34:06 -0300

    Ajuste função separação treino teste para incluir parametro stratify

--- src/black_mamba_pd/pipelines/PreparacaoDados/nodes.py
+++ src/black_mamba_pd/pipelines/PreparacaoDados/nodes.py
@@ -12,24 +12,39 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 def load_data_kobe(data):
+    """
+    Leitura dos dados iniciais disponibilizados pelo Kaggle e filtro 
+    das informações que serão utilizadas na modelagem.       
+    """
     data = data[data['shot_type'] == '2PT Field Goal']
     data = data[['lat','lon','minutes_remaining', 'period', 'playoffs', 'shot_distance','shot_made_flag']]
     return data
 
 def generate_base_inference(data):
+    """
+    Geração dos dados que serão utilizados para inferência do modelo.
+    """
     data = data[data['shot_type'] == '3PT Field Goal']
     data = data[['lat','lon','minutes_remaining', 'period', 'playoffs', 'shot_distance','shot_made_flag']]
     data = data.dropna()
     return data
 
 def conform_data_kobe(data):
+    """
+    Conformação dos dados para utilização na modelagem.
+    """
     data = data.dropna()
     return data   
 
 def train_test_split_data_kobe(data, n_test_size, n_random_state):
+    """
+    Separação das bases de treino e teste.
+    """
+    
     X = data.drop(columns=['shot_made_flag']).copy()
     y = data['shot_made_flag']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=n_test_size, random_state = n_random_state)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=n_test_size, 
+                                                        random_state = n_random_state, stratify=y)
     base_train = pd.merge(X_train, y_train, left_index=True, right_index=True)
     base_test = pd.merge(X_test, y_test, left_index=True, right_index=True)
     
@@ -40,6 +55,9 @@ def train_test_split_data_kobe(data, n_test_size, n_random_state):
     return base_train, base_test
 
 def metrics_preparacao_dados(data, data_train, data_test):
+    """
+    Registro no MLFlow dos tamanhos das bases de treino e teste.
+    """
     LOGGER.info(f"Tamanho total da base: {data.shape[0]}")
     LOGGER.info(f"Tamanho da base de treino: {data_train.shape[0]}")
     LOGGER.info(f"Tamanho da base de teste: {data_test.shape[0]}")
